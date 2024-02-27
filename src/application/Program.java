14b29e7df625bc007bc3cb971dfa9fa3c27dfd91
pedro-batista <peddrobatista2022@gmail.com>
2024-02-27 13:06:06 -0300

    Segundo commit...

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program {
 	
@@ -17,7 +18,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 	}
