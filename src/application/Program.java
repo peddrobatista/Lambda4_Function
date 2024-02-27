f1a86aff98411812e1d1807889305c1069496d33
pedro-batista <peddrobatista2022@gmail.com>
2024-02-27 15:29:03 -0300

    Quinto commit...

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,10 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
 
 public class Program {
 	
@@ -17,8 +17,10 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
+		
+		Function<Product, String> func = p -> p.getName().toUpperCase(); 
 
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 	}
